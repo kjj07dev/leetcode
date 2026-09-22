d7d0e47c3564a8959209c832c9644a9d3012b77c
kjj07dev <kjj07dev@gmail.com>
2026-09-22 21:03:56 +0000

    .

---
+++ 150.cpp
@@ -0,0 +1,29 @@
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(string token: tokens) {
+            if (token != "+" && token != "-" && token != "*" &&token != "/") {
+                st.push(stoi(token));
+            }
+            else {
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
+
+                if(token == "+") st.push(a+b);
+                else if (token == "-") st.push(a-b);
+                else if (token == "*") st.push(a*b);
+                else if (token == "/") st.push(a/b);
+            }
+        }
+        return st.top();
+    }
+};
